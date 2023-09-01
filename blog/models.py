9a88b231da024b8a0b717aa64c477e73682f0df6
MRBA86 <m.r.b2769973@gmail.com>
2023-09-02 01:12:28 +0330

    Create 2 models in blog and website
    Add Template Draw SQL Project
    Update Post model in Blog app
    Create Contact Model in Website app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Post(models.Model):
+    #author
+    #image
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    #tag
+    #category
+    counted_views = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+
 # Create your models here.
--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
 # Create your models here.
