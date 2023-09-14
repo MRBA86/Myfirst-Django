a1f8e201b0995f9e471fb4dba4a3077f612bd41a
MRBA86 <m.r.b2769973@gmail.com>
2023-09-14 16:55:16 +0330

    make single post Dynamic
    change path in url.py blog
    filter post with date published
    send pid to single post

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
+from datetime import datetime as dt
 
 def blog_view(request):
-    return render(request,'blog/blog-home.html')
+    posts =Post.objects.exclude(published_date__gte = dt.now())
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post':post}
+    return render(request, 'blog/blog-single.html', context)
+
+
+def test(request):
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
+    return render(request,'blog/test.html', context)
 # Create your views here.
