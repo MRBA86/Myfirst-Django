03d14c68f9fe3d56db77bf94e3c250bb506402e0
MRBA86 <m.r.b2769973@gmail.com>
2023-09-15 21:10:52 +0330

    change module time zone django istead of date time

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
-from datetime import datetime as dt
+from django.utils import timezone as tz
+
 
 def blog_view(request):
-    posts =Post.objects.exclude(published_date__gte = dt.now())
+    posts =Post.objects.exclude(published_date__gte = tz.now())
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request, pid):
-    posts =Post.objects.exclude(published_date__gte = dt.now())
+    posts =Post.objects.exclude(published_date__gte = tz.now())
     post = get_object_or_404(posts, pk=pid)
     post.counted_views = post.counted_views + 1
     post.save()
