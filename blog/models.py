820254954ab02c01e510eba77c8ef8c8369935ad
MRBA86 <m.r.b2769973@gmail.com>
2023-09-16 01:18:57 +0330

    Add Category model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category (models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
--- blog/views.py
+++ blog/views.py
@@ -11,14 +11,11 @@ def blog_view(request):
 def blog_single(request, pid):
     posts =Post.objects.exclude(published_date__gte = tz.now())
     post = get_object_or_404(posts, pk=pid)
-    post.counted_views = post.counted_views + 1
-    post.save()
-    context = {'post':post}
+    list_post = list(posts)
+    #post.counted_views = post.counted_views + 1
+    #post.save()
+    index_post = list_post.index(post)
+    context = {'post':post,'ind':index_post}
     return render(request, 'blog/blog-single.html', context)
 
-
-def test(request):
-    posts = Post.objects.filter(status=1)
-    context = {'posts':posts}
-    return render(request,'blog/test.html', context)
 # Create your views here.
