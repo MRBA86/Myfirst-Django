7da19d3db0d98bcd64c8f8a658fac377c71e5740
MRBA86 <m.r.b2769973@gmail.com>
2023-09-29 00:46:13 +0330

    Create Search form and suitable view and url

--- blog/views.py
+++ blog/views.py
@@ -30,4 +30,13 @@ def blog_category(request,cat_name):
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
+def blog_search(request):
+    posts =Post.objects.filter(published_date__lte= tz.now() , status = 1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains = s)
+
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
+
 # Create your views here.
