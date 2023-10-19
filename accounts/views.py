289430751425359c478d30ff53f3f6175b5747b4
MRBA86 <m.r.b2769973@gmail.com>
2023-10-20 00:42:47 +0330

    Add Signup Views and template with complete comment section

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,45 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate , login , logout 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+# !1!from django.contrib import messages
 
 
 def login_view(request):
-    if request.user.is_authenticated:
-        msg = f'user is authenticated as {request.user.username}'
-    else :
-        msg = 'user is not authenticated'
-    
-    
-    return render(request,'accounts/login.html',{'msg':msg})
-
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            # !1! else :
+            # !1!    messages.ERROR(request, "Username Or Password is not valid")  
+        return render(request,'accounts/login.html')
+    else:
+        return redirect('/')
+@login_required()
 def logout_view(request):
+    logout(request)
     return redirect('/')
+    #if request.user.is_authenticated:
+    #    logout(request)
+    #return redirect('/')
 
 def signup_view(request):
-    return render(request,'accounts/signup.html')
+    if not request.user.is_authenticated :
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/accounts/login')
+
+        form = UserCreationForm()
+        context = {'form':form}
+        return render(request,'accounts/signup.html',context)
+    else :
+        return redirect('/')
 
 # Create your views here.
--- mysite/settings.py
+++ mysite/settings.py
@@ -166,3 +166,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+LOGIN_URL='/accounts/login'
